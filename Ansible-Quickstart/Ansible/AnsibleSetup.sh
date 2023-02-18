b91ce1d3609199691005020b36f8216f885ed806
ndy5000 <62970064+ndy5000@users.noreply.github.com>
2023-02-17 21:47:35 -0600

    Update AnsibleSetup.sh

--- Ansible-Quickstart/Ansible/AnsibleSetup.sh
+++ Ansible-Quickstart/Ansible/AnsibleSetup.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # Update all packages that have available updates.
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo yum update -y
 
 # Install Python 3 and pip.
-sudo apt-get install python3-pip -y
+sudo yum install -y python3-pip
 
 # Upgrade pip3.
 sudo pip3 install --upgrade pip
 
-# Install Ansible 
-sudo pip3 install 'ansible[azure]'
+# Install Ansible.
+pip3 install "ansible==2.9.17"
 
-# Install Ansible az collection for interacting with Azure.
-ansible-galaxy collection install azure.azcollection
-
-# Install Ansible modules for Azure
-sudo pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
+# Install Ansible azure_rm module for interacting with Azure.
+pip3 install ansible[azure]
